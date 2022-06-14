ef22645e27dc1c18bab4541b2fd334aab180f1ce
Ilie0505 <80155422+Ilie0505@users.noreply.github.com>
2022-06-14 19:10:29 +0300

    14

---
+++ downloads/mixitup/script.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('#MixItUp1').mixItUp({
+    selectors: {
+      filter: '.filter-1',
+      sort: '.sort-1'
+    }
+  });
+  
+  $('#MixItUp2').mixItUp({
+    selectors: {
+      filter: '.filter-2',
+      sort: '.sort-2'
+    }
+  });
+});
